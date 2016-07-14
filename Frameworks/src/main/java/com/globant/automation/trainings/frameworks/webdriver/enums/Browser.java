cc3c9899c5feb439c72d4d2f2c333f2232997b20
Juan Krzemien <juan.krzemien@globant.com>
2016-07-14 16:08:43 -0300

    Replaced conditional with polymorphism...Shame on me...

--- Frameworks/src/main/java/com/globant/automation/trainings/frameworks/webdriver/enums/Browser.java
+++ Frameworks/src/main/java/com/globant/automation/trainings/frameworks/webdriver/enums/Browser.java
@@ -1,5 +1,8 @@
 package com.globant.automation.trainings.frameworks.webdriver.enums;
 
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 /**
  * Enumeration that defines the browsers supported by this framework
  *
@@ -7,6 +10,55 @@ package com.globant.automation.trainings.frameworks.webdriver.enums;
  */
 public enum Browser {
 
-    FIREFOX, CHROME, IE, SAFARI, ANDROID, IPHONE, IPAD
+    FIREFOX {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.firefox();
+        }
+    },
+    CHROME {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.chrome();
+        }
+    },
+    IE {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.internetExplorer();
+        }
+    },
+    EDGE {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.edge();
+        }
+    },
+    SAFARI {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.safari();
+        }
+    },
+    ANDROID {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.android();
+        }
+    },
+    IPHONE {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.iphone();
+        }
+    },
+    IPAD {
+        @Override
+        public Capabilities getCapabilities() {
+            return DesiredCapabilities.ipad();
+        }
+    };
+
+    public abstract Capabilities getCapabilities();
 
 }
--- Frameworks/src/main/java/com/globant/automation/trainings/frameworks/webdriver/factory/WebDriverStrategyFactory.java
+++ Frameworks/src/main/java/com/globant/automation/trainings/frameworks/webdriver/factory/WebDriverStrategyFactory.java
@@ -6,55 +6,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
-import org.slf4j.Logger;
 
 import java.util.Map;
 
 import static com.globant.automation.trainings.frameworks.webdriver.config.Framework.CONFIGURATION;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.openqa.selenium.remote.CapabilityType.PROXY;
-import static org.slf4j.LoggerFactory.getLogger;
 
 /**
  * @author Juan Krzemien
  */
 public class WebDriverStrategyFactory {
 
-    private static final Logger LOG = getLogger(WebDriverStrategyFactory.class);
-
     public WebDriver getDriverFor(Browser browser) {
 
-        // Default is Firefox
-        DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-
-        switch (browser) {
-            case CHROME:
-                capabilities = DesiredCapabilities.chrome();
-                break;
-            case IE:
-                capabilities = DesiredCapabilities.internetExplorer();
-                break;
-            case SAFARI:
-                capabilities = DesiredCapabilities.safari();
-                break;
-            case ANDROID:
-                capabilities = DesiredCapabilities.android();
-                break;
-            case IPHONE:
-                capabilities = DesiredCapabilities.iphone();
-                break;
-            case IPAD:
-                capabilities = DesiredCapabilities.ipad();
-                break;
-        }
+        DesiredCapabilities capabilities = new DesiredCapabilities(browser.getCapabilities());
 
         Map<String, Object> driverConfig = CONFIGURATION.Driver(browser).getCapabilities();
 
         capabilities.merge(new DesiredCapabilities(driverConfig));
 
-        /**
-         * Set proxy settings, if any
-         */
+        // Set proxy settings, if any
         if (CONFIGURATION.Proxy().isEnabled()) {
             capabilities.setCapability(PROXY, CONFIGURATION.Proxy().getHost() + ":" + CONFIGURATION.Proxy().getPort());
         }
@@ -63,9 +35,7 @@ public class WebDriverStrategyFactory {
     }
 
     public WebDriver getDriverFor(DesiredCapabilities capabilities) {
-        /**
-         * Create driver
-         */
+        // Create driver
         WebDriver driver = new RemoteWebDriver(capabilities);
 
         driver.manage().timeouts().implicitlyWait(CONFIGURATION.WebDriver().getImplicitTimeOut(), SECONDS);
@@ -73,17 +43,12 @@ public class WebDriverStrategyFactory {
         driver.manage().timeouts().setScriptTimeout(CONFIGURATION.WebDriver().getScriptTimeout(), SECONDS);
 
         if (CONFIGURATION.WebDriver().isUseListener()) {
-            /**
-             * Wrap the driver as an event firing one, and add basic listener
-             */
+            // Wrap the driver as an event firing one, and add basic listener
             final EventFiringWebDriver eventDriver = new EventFiringWebDriver(driver);
             eventDriver.register(new BasicWebDriverListener());
             return eventDriver;
         }
 
-        /**
-         * Finally, return the driver instance
-         */
         return driver;
     }
 
