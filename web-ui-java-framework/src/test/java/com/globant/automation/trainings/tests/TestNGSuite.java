d55bf4a86b06c3d8eecbc82efcfb00841729e081
Juan Krzemien <juan.krzemien@globant.com>
2017-02-02 14:45:23 -0300

    Merged Web UI related code into a single module. Removed useless ones.

--- web-ui-java-framework/src/test/java/com/globant/automation/trainings/tests/TestNGSuite.java
+++ web-ui-java-framework/src/test/java/com/globant/automation/trainings/tests/TestNGSuite.java
@@ -1,6 +1,7 @@
 package com.globant.automation.trainings.tests;
 
 import com.globant.automation.trainings.logging.Logging;
+import com.globant.automation.trainings.pages.GoogleHomePage;
 import com.globant.automation.trainings.pages.SamplePage2;
 import com.globant.automation.trainings.webdriver.tests.WebDriverTestNGListener;
 import org.testng.annotations.Listeners;
@@ -13,10 +14,17 @@ import org.testng.annotations.Test;
 public class TestNGSuite implements Logging {
 
     SamplePage2 samplePage;
+    GoogleHomePage home;
 
     @Test
     public void aSampleTest() throws Exception {
         getLogger().info("Current POM is " + samplePage.toString());
+    }
+
+    @Test
+    public void aWebDriverTest() throws Exception {
+        home.open();
+        home.search("Something something dark side...").getResultsTexts();
     }
 
 }
